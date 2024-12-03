7a7194846f3a807592f00658b4f92c374ca98825
willjcim <will@aptlabs.net>
2024-12-02 23:38:47 -0500

    cleanup and optimization, p1 6ms, p2 8ms

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 
-
 // https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
 template <typename Out>
 void split(const string &s, char delim, Out result) {
@@ -24,8 +23,8 @@ vector<int> split(const string &s, char delim) {
     vector<string> sLevels;
     vector<int> iLevels;
     split(s, delim, back_inserter(sLevels));
-    transform(sLevels.begin(), sLevels.end(), std::back_inserter(iLevels),
-               [](const std::string& str) { return std::stoi(str); });
+    transform(sLevels.begin(), sLevels.end(), back_inserter(iLevels),
+               [](const string& str) { return stoi(str); });
     return iLevels;
 }
 
@@ -52,6 +51,64 @@ bool checkSafety(vector<int> report, int pop) {
     return true;
 }
 
+
+// Solves Day 2 Part 1
+// O (n * m) or O (n^2)
+int partOne(string inputText) {
+    int safeReports = 0;
+    
+    istringstream iss(inputText);
+    string str;
+    while (getline(iss, str)) {
+        vector<int> report = split(str, ' ');
+
+        bool safe = true;
+        // true for increment
+        // false for decrement
+        bool direction = (report[0] < report[1]); 
+        for (int i = 0; i < report.size() - 1; i++) {
+            if ((report[i] < report[i+1]) != direction) {
+                safe = false;
+            }
+            if (report[i] == report[i+1]) {
+                safe = false;
+            } else if (abs(report[i] - report[i+1]) > 3) {
+                safe = false;
+            }
+        }
+        safeReports += safe;   
+    }
+    return safeReports;
+}
+
+
+// Solves Day 2 Part 2
+// O (n * m^2) or O (n^3)
+int partTwo(string inputText) {
+    int safeReports = 0;
+    
+    istringstream iss(inputText);
+    string str;
+    while (getline(iss, str)) {
+        vector<int> report = split(str, ' ');
+        bool safe = false;
+        
+        for (int i = 0; i < report.size(); i++) {
+            safe = checkSafety(report, i);
+            if (safe) {
+                break;
+            }
+        }
+        safeReports += safe;
+    }
+    return safeReports;
+}
+
+
+
+
+// IDEA GRAVEYARD
+// This really should have worked :(
 
 // int partTwo(string inputText) {
 //     int safeReports = 0;
@@ -90,57 +147,3 @@ bool checkSafety(vector<int> report, int pop) {
 //     return 1;
 // }
 
-
-
-// Solves Day 2 Part 2
-int partTwo(string inputText) {
-    int safeReports = 0;
-    
-    istringstream iss(inputText);
-    string str;
-    while (getline(iss, str)) {
-        vector<int> report = split(str, ' ');
-        bool safe = false;
-        
-        for (int i = 0; i < report.size(); i++) {
-            safe = checkSafety(report, i);
-            if (safe) {
-                break;
-            }
-        }
-        safeReports += safe;
-    }
-    cout << "Part 2: " << safeReports << "\n";
-    return 1;
-}
-
-
-
-// Solves Day 2 Part 1
-int partOne(string inputText) {
-    int safeReports = 0;
-    
-    istringstream iss(inputText);
-    string str;
-    while (getline(iss, str)) {
-        vector<int> report = split(str, ' ');
-
-        bool safe = true;
-        // true for increment
-        // false for decrement
-        bool direction = (report[0] < report[1]); 
-        for (int i = 0; i < report.size() - 1; i++) {
-            if ((report[i] < report[i+1]) != direction) {
-                safe = false;
-            }
-            if (report[i] == report[i+1]) {
-                safe = false;
-            } else if (abs(report[i] - report[i+1]) > 3) {
-                safe = false;
-            }
-        }
-        safeReports += safe;   
-    }
-    cout << "Part 1: " << safeReports << "\n";
-    return 1;
-}
--- day2/stubb.cpp
+++ day2/stubb.cpp
@@ -8,8 +8,10 @@ int main() {
                          istreambuf_iterator<char>());
 
     auto part1 = timer<decltype(&partOne)>::time(partOne, content);
+    cout << "Part 1: " << part1 << "\n";
 
     auto part2 = timer<decltype(&partTwo)>::time(partTwo, content);
+    cout << "Part 2: " << part2 << "\n";
 
 	return 1;
 }
